ca93f6b6568cf621834ed6dd676901586981f1a6
arshadzahangirchowdhury <arshad.zahangir.bd@gmail.com>
2022-03-03 21:51:46 -0600

    ADD:2018 ROI_Finder updated

--- beamtime_workflows/ROI_Finder_2018.py
+++ beamtime_workflows/ROI_Finder_2018.py
@@ -5,8 +5,8 @@
 Essential ROI-finder classes and functions
 """
 
-import beamtime_config
-from beamtime_config import *
+import beamtime_config_2018
+from beamtime_config_2018 import *
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from numpy import matlib
@@ -16,6 +16,9 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from matplotlib import rc
+from matplotlib.widgets import LassoSelector
+from matplotlib.path import Path
+
 
 class XRF_image:
     
@@ -223,6 +226,7 @@ class XRF_image:
         self.binary_img=[]
         self.region_vals=[]
         self.features_list = []
+        self.center_coords=[]
 
         for idx in range(len(self.regions)):
             self.cell_val_bin=self.regions[idx].image
@@ -382,3 +386,68 @@ def calc_SNR(img, seg_img, labels = (0,1), mask_ratio = None):
         return 1/(std + eps)
     else:
         return 1/(np.std(img) + eps)
+    
+    
+    
+class SelectFromCollection:
+    """
+    Select indices from a matplotlib collection using `LassoSelector`.
+
+    Selected indices are saved in the `ind` attribute. This tool fades out the
+    points that are not part of the selection (i.e., reduces their alpha
+    values). If your collection has alpha < 1, this tool will permanently
+    alter the alpha values.
+
+    Note that this tool selects collection objects based on their *origins*
+    (i.e., `offsets`).
+
+    Parameters
+    ----------
+    ax : `~matplotlib.axes.Axes`
+        Axes to interact with.
+    collection : `matplotlib.collections.Collection` subclass
+        Collection you want to select from.
+    alpha_other : 0 <= float <= 1
+        To highlight a selection, this tool sets all selected points to an
+        alpha value of 1 and non-selected points to *alpha_other*.
+    """
+
+    def __init__(self, ax, collection, alpha_other=0.3):
+        self.canvas = ax.figure.canvas
+        self.collection = collection
+        self.alpha_other = alpha_other
+
+        self.xys = collection.get_offsets()
+        self.Npts = len(self.xys)
+
+        # Ensure that we have separate colors for each object
+        self.fc = collection.get_facecolors()
+        if len(self.fc) == 0:
+            raise ValueError('Collection must have a facecolor')
+        elif len(self.fc) == 1:
+            self.fc = np.tile(self.fc, (self.Npts, 1))
+
+        self.lasso = LassoSelector(ax, onselect=self.onselect)
+        self.ind = []
+
+    def onselect(self, verts):
+        path = Path(verts)
+        self.ind = np.nonzero(path.contains_points(self.xys))[0]
+        self.fc[:, -1] = self.alpha_other
+        self.fc[self.ind, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
+
+    def disconnect(self):
+        self.lasso.disconnect_events()
+        self.fc[:, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
+        
+def accept(event):
+    if event.key == "enter":
+        print("Selected points:")
+        print(selector.xys[selector.ind])
+        selector.disconnect()
+        ax.set_title("")
+        fig.canvas.draw()
---
+++ beamtime_workflows/XRF_dataset_2018.py
@@ -0,0 +1,363 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Loads 2018 XRF data for ecoli bacteria. This data can be used to train PCA,k-means, supervised neural nets
+
+'''
+
+import beamtime_config_2018
+import ROI_Finder_2018
+from beamtime_config_2018 import *
+from ROI_Finder_2018 import *
+
+
+noise_type = 'none'
+base_file_path='/data02/AZC/XRF_Data/'
+
+def load_XRF_dataset_2018(noise_type = 'none', base_file_path='/data01/AZC/XRF_Data/' ):
+    
+    '''
+    loads 2018 XRF data, converts to binary and extract patches based on most relevant conversion parameters found.
+    
+    Arguments:
+    ----------
+    noise_type: string, 'poisson' argunment will add noise defned in ROI-Finder_2018.
+    base_file_path: string, path to folder containing XRF data
+    
+    Returns:
+    --------
+    data_dict:dict, contains handmade features
+    
+    '''
+    
+    #file 1
+
+    x= XRF_image(xrf_filename = base_file_path + 'bnp_fly0001_2018_1.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+
+    x.binary_conversion(e='Cu')
+    x.extract_cells()
+    X_bin1=x.binary_img  
+
+
+    X_Cu1=x.Patches_Cu
+    X_Zn1=x.Patches_Zn
+    X_Ca1=x.Patches_Ca
+    X_K1=x.Patches_K
+    X_P1=x.Patches_P
+    X_S1=x.Patches_S
+    X_Fe1=x.Patches_Fe
+    X_Ni1=x.Patches_Ni
+    X_TFY1=x.Patches_TFY
+
+    SNR_X1=calc_SNR(x.d_Cu, x.binary_ero_dil)
+
+    X1=x.features
+    X1.shape
+
+    #file 2
+
+
+    x= XRF_image(xrf_filename = base_file_path + 'bnp_fly0001_2018_3.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+    x.binary_conversion(e='K')
+    x.extract_cells()
+    X_bin2=x.binary_img
+
+    X_Cu2=x.Patches_Cu
+    X_Zn2=x.Patches_Zn
+    X_Ca2=x.Patches_Ca
+    X_K2=x.Patches_K
+    X_P2=x.Patches_P
+    X_S2=x.Patches_S
+    X_Fe2=x.Patches_Fe
+    X_Ni2=x.Patches_Ni
+    X_TFY2=x.Patches_TFY
+
+
+    SNR_X2=calc_SNR(x.d_K, x.binary_ero_dil)
+
+    X2=x.features
+    X2.shape
+
+    #file 3
+
+    x= XRF_image(xrf_filename = base_file_path + 'bnp_fly0003_2018_3.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+
+    x.binary_conversion(e='K')
+    x.extract_cells()
+    X_bin3=x.binary_img
+
+    X_Cu3=x.Patches_Cu
+    X_Zn3=x.Patches_Zn
+    X_Ca3=x.Patches_Ca
+    X_K3=x.Patches_K
+    X_P3=x.Patches_P
+    X_S3=x.Patches_S
+    X_Fe3=x.Patches_Fe
+    X_Ni3=x.Patches_Ni
+    X_TFY3=x.Patches_TFY
+
+
+    SNR_X3=calc_SNR(x.d_K, x.binary_ero_dil)
+
+    X3=x.features
+    X3.shape
+
+    #file 4
+
+    x= XRF_image(xrf_filename = base_file_path + 'bnp_fly0012_2018_1w2.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+
+    x.binary_conversion(e='Cu')
+    x.extract_cells()
+    X_bin4=x.binary_img
+
+    X_Cu4=x.Patches_Cu
+    X_Zn4=x.Patches_Zn
+    X_Ca4=x.Patches_Ca
+    X_K4=x.Patches_K
+    X_P4=x.Patches_P
+    X_S4=x.Patches_S
+    X_Fe4=x.Patches_Fe
+    X_Ni4=x.Patches_Ni
+    X_TFY4=x.Patches_TFY
+
+    SNR_X4=calc_SNR(x.d_Cu, x.binary_ero_dil)
+
+    X4=x.features
+    X4.shape
+
+    #file 5
+
+    x= XRF_image(xrf_filename = base_file_path + 'bnp_fly0014_2018_1w2.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+
+    x.binary_conversion(e='Cu')
+    x.extract_cells()
+
+    X_bin5=x.binary_img
+
+    X_Cu5=x.Patches_Cu
+    X_Zn5=x.Patches_Zn
+    X_Ca5=x.Patches_Ca
+    X_K5=x.Patches_K
+    X_P5=x.Patches_P
+    X_S5=x.Patches_S
+    X_Fe5=x.Patches_Fe
+    X_Ni5=x.Patches_Ni
+    X_TFY5=x.Patches_TFY
+
+    SNR_X5=calc_SNR(x.d_Cu, x.binary_ero_dil)
+
+
+    X5=x.features
+    X5.shape
+
+    #file 6
+
+    x= XRF_image(xrf_filename = base_file_path + 'bnp_fly0040_2018_1.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+
+    x.binary_conversion(e='Cu')
+    x.extract_cells()
+    X_bin6=x.binary_img
+
+    X_Cu6=x.Patches_Cu
+    X_Zn6=x.Patches_Zn
+    X_Ca6=x.Patches_Ca
+    X_K6=x.Patches_K
+    X_P6=x.Patches_P
+    X_S6=x.Patches_S
+    X_Fe6=x.Patches_Fe
+    X_Ni6=x.Patches_Ni
+    X_TFY6=x.Patches_TFY
+
+    SNR_X6=calc_SNR(x.d_Cu, x.binary_ero_dil)
+
+    X6=x.features
+    X6.shape
+
+    #file 7
+
+    x= XRF_image(xrf_filename = '/data01/AZC/XRF_Data/bnp_fly0050_2018_1.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+
+    x.binary_conversion(e='Cu')
+    x.extract_cells()
+    X_bin7=x.binary_img
+
+    X_Cu7=x.Patches_Cu
+    X_Zn7=x.Patches_Zn
+    X_Ca7=x.Patches_Ca
+    X_K7=x.Patches_K
+    X_P7=x.Patches_P
+    X_S7=x.Patches_S
+    X_Fe7=x.Patches_Fe
+    X_Ni7=x.Patches_Ni
+    X_TFY7=x.Patches_TFY
+
+    SNR_X7=calc_SNR(x.d_Cu, x.binary_ero_dil)
+
+    X7=x.features
+    X7.shape
+
+    #file 8
+
+    x= XRF_image(xrf_filename = base_file_path + 'bnp_fly0051_2018_1.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+
+    x.binary_conversion(e='Cu')
+    x.extract_cells()
+    X_bin8=x.binary_img
+
+
+    X_Cu8=x.Patches_Cu
+    X_Zn8=x.Patches_Zn
+    X_Ca8=x.Patches_Ca
+    X_K8=x.Patches_K
+    X_P8=x.Patches_P
+    X_S8=x.Patches_S
+    X_Fe8=x.Patches_Fe
+    X_Ni8=x.Patches_Ni
+    X_TFY8=x.Patches_TFY
+
+    SNR_X8=calc_SNR(x.d_Cu, x.binary_ero_dil)
+
+    X8=x.features
+    X8.shape
+
+    # file 9
+
+    x= XRF_image(xrf_filename = base_file_path + 'bnp_fly0052_2018_1.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+
+    x.binary_conversion(e='Cu')
+    x.extract_cells()
+    X_bin9=x.binary_img
+
+    X_Cu9=x.Patches_Cu
+    X_Zn9=x.Patches_Zn
+    X_Ca9=x.Patches_Ca
+    X_K9=x.Patches_K
+    X_P9=x.Patches_P
+    X_S9=x.Patches_S
+    X_Fe9=x.Patches_Fe
+    X_Ni9=x.Patches_Ni
+    X_TFY9=x.Patches_TFY
+
+    SNR_X9=calc_SNR(x.d_Cu, x.binary_ero_dil)
+
+    X9=x.features
+    X9.shape
+
+    #file 10
+    x= XRF_image(xrf_filename = base_file_path + 'bnp_fly0065_2018_3.h5',
+                 BASE_PATCH_WIDTH=32, verbosity=False)
+    x.load_xrf_data(hdf5_string='exchange_4')
+    x.add_noise(noise=noise_type)
+    x.binary_conversion(e='K')
+    x.extract_cells()
+    X_bin10=x.binary_img
+
+    X_Cu10=x.Patches_Cu
+    X_Zn10=x.Patches_Zn
+    X_Ca10=x.Patches_Ca
+    X_K10=x.Patches_K
+    X_P10=x.Patches_P
+    X_S10=x.Patches_S
+    X_Fe10=x.Patches_Fe
+    X_Ni10=x.Patches_Ni
+    X_TFY10=x.Patches_TFY
+
+
+    SNR_X10=calc_SNR(x.d_K, x.binary_ero_dil)
+
+    X10=x.features
+    X10.shape
+
+    X=np.concatenate((X1,X2,X3,X4,X5,X6,X7,X8,X9,X10))
+    X_bin=np.concatenate((X_bin1,X_bin2,X_bin3,X_bin4,X_bin5,X_bin6,X_bin7,X_bin8,X_bin9,X_bin10))
+    X_Cu=np.concatenate((X_Cu1,X_Cu2,X_Cu3,X_Cu4,X_Cu5,X_Cu6,X_Cu7,X_Cu8,X_Cu9,X_Cu10))
+    X_Zn=np.concatenate((X_Zn1,X_Zn2,X_Zn3,X_Zn4,X_Zn5,X_Zn6,X_Zn7,X_Zn8,X_Zn9,X_Zn10))
+    X_Ca=np.concatenate((X_Ca1,X_Ca2,X_Ca3,X_Ca4,X_Ca5,X_Ca6,X_Ca7,X_Ca8,X_Ca9,X_Ca10))
+    X_K=np.concatenate((X_K1,X_K2,X_K3,X_K4,X_K5,X_K6,X_K7,X_K8,X_K9,X_K10))
+    X_P=np.concatenate((X_P1,X_P2,X_P3,X_P4,X_P5,X_P6,X_P7,X_P8,X_P9,X_P10))
+    X_S=np.concatenate((X_S1,X_S2,X_S3,X_S4,X_S5,X_S6,X_S7,X_S8,X_S9,X_S10))
+    X_Fe=np.concatenate((X_Fe1,X_Fe2,X_Fe3,X_Fe4,X_Fe5,X_Fe6,X_Fe7,X_Fe8,X_Fe9,X_Fe10))
+    X_Ni=np.concatenate((X_Ni1,X_Ni2,X_Ni3,X_Ni4,X_Ni5,X_Ni6,X_Ni7,X_Ni8,X_Ni9,X_Ni10))
+    X_TFY=np.concatenate((X_TFY1,X_TFY2,X_TFY3,X_TFY4,X_TFY5,X_TFY6,X_TFY7,X_TFY8,X_TFY9,X_TFY10))
+
+    SNR=np.array([SNR_X1,SNR_X2,SNR_X3,SNR_X4,SNR_X5,SNR_X6,SNR_X7,SNR_X8,SNR_X9,SNR_X10])
+    SNR_Df = pd.DataFrame()
+    SNR_Df['SNR']=SNR
+
+    # print(SNR_Df.to_string())
+
+    print('Total extracted cells, features:', X.shape)
+    print('Total extracted cell, cell size:', X_bin.shape)
+
+    principalDf = pd.DataFrame(
+                 columns = ['Pixel_count', 'area'])
+
+    principalDf['area'] = X[:,0]
+    principalDf['eccentricity'] = X[:,1]
+    principalDf['equivalent_diameter'] = X[:,2]
+    principalDf['major_axis_length'] = X[:,3]
+    principalDf['minor_axis_length'] = X[:,4]
+    principalDf['perimeter'] = X[:,5]
+    principalDf['K'] = X[:,6]
+    principalDf['P'] = X[:,7]
+    principalDf['Ni'] = X[:,8]
+    principalDf['Zn'] = X[:,9]
+    principalDf['Fe'] = X[:,10]
+    principalDf['Cu'] = X[:,11]
+    principalDf['BFY'] = X[:,12]
+    principalDf['Pixel_count'] = X[:,13].astype(int)
+    # display(principalDf)
+    
+    annotation_file_path=base_file_path + 'xrf_annotations_arshad_KO_full_indices.csv'
+    KO_annotations = pd.read_csv(annotation_file_path)
+
+    principalDf['KO_label']=KO_annotations['KO_label'].to_numpy()
+    principalDf['K_obs_labels']=KO_annotations['K'].to_numpy()
+    principalDf['Ni_obs_labels']=KO_annotations['Ni'].to_numpy()
+    principalDf['Fe_obs_labels']=KO_annotations['Fe'].to_numpy()
+    principalDf['Cu_obs_labels']=KO_annotations['Cu'].to_numpy()
+    principalDf['Ca_obs_labels']=KO_annotations['Ca'].to_numpy()
+    principalDf['Zn_obs_labels']=KO_annotations['Zn'].to_numpy()
+
+    principalDf.KO_label = principalDf.KO_label.fillna('not_annotated')
+    principalDf.K_obs_labels = principalDf.K_obs_labels.fillna('not_annotated')
+    principalDf.Ni_obs_labels = principalDf.Ni_obs_labels.fillna('not_annotated')
+    principalDf.Fe_obs_labels = principalDf.Fe_obs_labels.fillna('not_annotated')
+    principalDf.Cu_obs_labels = principalDf.Cu_obs_labels.fillna('not_annotated')
+    principalDf.Ca_obs_labels = principalDf.Ca_obs_labels.fillna('not_annotated')
+    principalDf.Zn_obs_labels = principalDf.Zn_obs_labels.fillna('not_annotated')    
+    
+    
+    data_dict = {'X':X, 'X_bin':X_bin, 'X_Cu':X_Cu, 'X_Zn':X_Zn, 'X_Ca':X_Ca, 'X_K':X_K,
+                 'X_P':X_P,'X_S':X_S,'X_Fe':X_Fe,'X_Ni':X_Ni, 'X_TFY':X_TFY, 'principalDf':principalDf}
+    
+    return data_dict
+    
